0c6be86019e5865a9502711772445ba5887ca42e
Raúl Morales <raulmdaw@gmail.com>
2024-02-11 13:47:51 +0100

    393. @for - @if - Nueva sintaxis

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css',
 })
