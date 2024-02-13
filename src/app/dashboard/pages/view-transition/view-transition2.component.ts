7a2b78eeeb7266012a86f03528e618b687451f1e
Raúl Morales <raulmdaw@gmail.com>
2024-02-13 21:41:56 +0100

    405. Hero Animation - View Transitions

--- src/app/dashboard/pages/view-transition/view-transition2.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -6,16 +6,19 @@ import { TitleComponent } from '@shared/title/title.component';
   imports: [TitleComponent],
   template: `
     <app-title title="View Transition 2" />
-    <section class="flex justify-start">
+    <section class="flex justify-end">
       <img
         srcset="https://picsum.photos/id/237/200/300"
         alt="Picsum"
         width="200"
         height="300"
+        style="view-transition-name: hero1"
       >
 
-      <div class="bg-blue-500 w-56 h-56">
-
+      <div
+        class="fixed bottom-16 right-10 bg-blue-800 w-32 h-32 rounded"
+        style="view-transition-name: hero2"
+      >
       </div>
     </section>
   `
